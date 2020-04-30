b06e883c151239aeed82b3dd07cbb948686b50dd
ChidiChuks <chukschiboy@gmail.com>
2020-04-30 12:38:43 +0100

    creating floating action button and persistent footer buttons widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('Building Layouts with Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello Flutter Layouts',
+            style: TextStyle(fontSize: 24.0, ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.lightbulb_outline),
+          onPressed: () {
+            print('You rang!');
+          },
+        ),
+        persistentFooterButtons: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add_comment), 
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_alarm),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_location),
+            onPressed: () {},
+          )
+        ],
       ),
     );
   }
